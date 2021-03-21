b283a8883be88733825e6135367ccfa887833c29
Koseng <26673978+Koseng@users.noreply.github.com>
2021-03-21 09:40:59 +0100

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# MSFSAddonVersionChecker
+# MSFS Addon Version Checker
+
+### Introduction
+This simple application shows the installed versions and online available version of configured addons and tools for the Microsoft Flight Simulator. So it can be used to get an overview whether new versions are available.
+
+Supported sources for addons and tools are:
+* flightsim.to
+* github.com
+
+### Installation and Execution
+Download the latest released version. Unpack to a folder. For configuration edit addons.xml. Execute checkAddons.exe.
+
+### Configuration
+For configuration edit addons.xml with a proper editor. E.g. "Notepad", "Editor", ["Notepad++"](https://notepad-plus-plus.org/downloads/) or ["Visual Studio Code"](https://code.visualstudio.com/)". 
+
+For each addon or tool add a new line in the xml file and adjust the attributes like name and url accordingly. Edit the parts between the "".
+
+* **name**: Enter the exact name of the addon folder in your community folder. For a tool just enter the name of the tool.
+* **url**: Enter the url to the flightsim or github page to the tool or addon like in the examples.
+* **installedVersion**: For tools enter you installed version manually. For addons in the community folder it is auto detected. You can also manually enter an installed version for addons. That can be useful if the addon maker does not properly update its version information.
+* **versionKey**: For github you can optionally add a versionKey. That is necessary if an addon maker does release different addons in the the same github repository like Working-Title.
+
+### Development information
+#### Execute Python Script
+If you want to run the python script directly:
+* Install [Python](https://www.python.org/downloads/). On Installation check box for Path inclusion.
+* Install additional libraries via command shell:
+    * `pip install requests`
+    * `pip install beautifulsoup4`
+    * `pip install pyinstaller`
+* Now switch to the directory and you can run the python script via `python checkAddons.py`. Also it is possible to create a batch file like checkAddons.bat which contains `python checkAddons.py`.
+
+#### Build Executable
+Comment out os.chdir(sys.path[0]) in checkAddons.py. Then run createExecutable.bat.
+
---
+++ src/checkAddons.py
@@ -0,0 +1,71 @@
+import requests, os, sys, json
+import dateutil.parser
+from xml.dom import minidom
+from bs4 import BeautifulSoup
+
+# Set execution folder to folder of .py file
+os.chdir(sys.path[0]) 
+# MS Store
+communityFolder = os.path.join(os.getenv("LOCALAPPDATA"), "Packages/Microsoft.FlightSimulator_8wekyb3d8bbwe/LocalCache/Packages/Community")
+if not os.path.exists(communityFolder):
+    communityFolder = os.path.join(os.getenv("APPDATA"), "Microsoft Flight Simulator/Packages/Community") # Steam Version
+if not os.path.exists(communityFolder):
+    communityFolder = os.path.join(os.getenv("LOCALAPPDATA"), "MSFSPackages/Community") # Box Version
+doc = minidom.parse("addons.xml")
+addons = doc.getElementsByTagName("addon")
+if doc.documentElement.hasAttribute("communityFolder"):  
+    communityFolder = doc.documentElement.getAttribute("communityFolder")  
+headers = {'user-agent': 'Mozilla/5.0'}
+
+print("")
+print("{:<35} {:<20} {:<20} {:<25}".format("Name", "Installed", "Online", "Release Date"))
+print("================================================================================================")
+for addon in addons:
+    addonName = addon.getAttribute("name")
+    manifestPath = os.path.join(communityFolder, addonName, "manifest.json") 
+    manifestJso = "Unavailable"
+    installedVersion = "Unavailable"
+    onlineVersion = "Unavailable"
+    onlineReleaseDate = "Unavailable" 
+    # Installed addon
+    if os.path.exists(manifestPath):
+        with open(manifestPath) as f: 
+            manifestJson = json.load(f)
+        installedVersion = manifestJson["package_version"]
+    url = addon.getAttribute("url")
+    # Flightsim.to
+    if "flightsim.to" in url:
+        r = requests.get(url, headers=headers, allow_redirects=True)
+        if(r.ok):
+            soup = BeautifulSoup(r.content, 'html.parser') 
+            headerDict = {}
+            th = soup.find_all('th')
+            for h in th:
+                if h.next_sibling.next_sibling:
+                    if h.text not in headerDict:
+                        headerDict[h.text] = h.next_sibling.next_sibling.text
+            if "Version" in headerDict:
+                onlineVersion = headerDict["Version"]
+            if "Last Updated" in headerDict:
+                onlineReleaseDate = headerDict["Last Updated"]
+    # Github.com
+    if "github.com" in url:
+        apiUrl = url.replace("https://github.com", "https://api.github.com/repos") + "?per_page=100"
+        r = requests.get(apiUrl, headers=headers, allow_redirects=True)
+        if(r.ok):
+            releasesJson = r.json()
+            versionJson = releasesJson[0]
+            if addon.hasAttribute("versionKey"):
+                versionKey = addon.getAttribute("versionKey")
+                versionJson = next((x for x in releasesJson if versionKey in x["tag_name"]), None)
+            if versionJson:
+                onlineVersion = versionJson["tag_name"]
+                dt = dateutil.parser.isoparse(versionJson["published_at"])
+                onlineReleaseDate = dt.strftime("%B %d, %Y")
+    if addon.hasAttribute("installedVersion"):
+        installedVersion = addon.getAttribute("installedVersion")
+    # Output
+    print("{:<35} {:<20} {:<20} {:<25}".format(addonName, installedVersion, onlineVersion, onlineReleaseDate))
+    print("------------------------------------------------------------------------------------------------")
+input("Press enter to exit.")
+
